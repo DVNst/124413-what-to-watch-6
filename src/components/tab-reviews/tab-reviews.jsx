8108ea74d0c1140b49eaf9ad5689d96ca5b0357c
DVNst <dvn-studio@mail.ru>
2021-02-22 15:23:23 +0300

    refactoring module TabReviews

--- src/components/tab-reviews/tab-reviews.jsx
+++ src/components/tab-reviews/tab-reviews.jsx
@@ -6,13 +6,19 @@ import {ReviewsTypes} from '../proptypes';
 import Review from '../review/review';
 
 const TabReviews = ({reviews}) => {
+  const reviewsHalf = Math.ceil(reviews.length / 2);
+  const reviewsLeft = reviews.slice(0, reviewsHalf);
+  const reviewsRight = reviews.slice(reviewsHalf);
+
+  const getReviews = (items) => items.map((review) => <Review key={review.id} review={review}/>);
+
   return (
     <div className="movie-card__reviews movie-card__row">
       <div className="movie-card__reviews-col">
-        {reviews.slice(0, Math.ceil(reviews.length / 2)).map((review) => <Review key={review.id} review={review}/>)}
+        {getReviews(reviewsLeft)}
       </div>
       <div className="movie-card__reviews-col">
-        {reviews.slice(Math.ceil(reviews.length / 2)).map((review) => <Review key={review.id} review={review}/>)}
+        {getReviews(reviewsRight)}
       </div>
     </div>
   );

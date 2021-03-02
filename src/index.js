28be292f73eafb783f9dd313cee98dfa3f4bc5c8
DVNst <dvn-studio@mail.ru>
2021-03-02 17:22:43 +0300

    add module catalog

--- src/index.js
+++ src/index.js
@@ -7,24 +7,11 @@ import {reducer} from './store/reducer';
 
 import App from './components/app/app';
 
-import {films} from './mocks/films.js';
-
-const Setting = {
-  MOVIE_PROMO_NAME: `The Grand Budapest Hotel`,
-  MOVIE_PROMO_GENRE: `Drama`,
-  MOVIE_PROMO_RELEASED: 2014,
-};
-
 const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
-      <App
-        moviePromoName={Setting.MOVIE_PROMO_NAME}
-        moviePromoGenre={Setting.MOVIE_PROMO_GENRE}
-        moviePromoReleased={Setting.MOVIE_PROMO_RELEASED}
-        movies={films}
-      />
+      <App />
     </Provider>,
     document.querySelector(`#root`)
 );
